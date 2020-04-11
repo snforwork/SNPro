e7986c1ccd71677bccb7f62d77151809cfb3a77d
ASUS <ASUS@DESKTOP-42OM71Q>
2020-04-11 18:49:23 +0900

    4.11宿題提出

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -3,17 +3,20 @@ package co.jp.chapter03;
 public class JavaChapter03 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 鑷嫊鐢熸垚銇曘倢銇熴儭銈姐儍銉夈兓銈广偪銉�
 
-		//   日本硬貨　１，５，１０，５０，１００，５００
-		// input 総額
-		// output 各額度の枚数
+		//   鏃ユ湰纭波銆�锛戯紝锛曪紝锛戯紣锛岋紩锛愶紝锛戯紣锛愶紝锛曪紣锛�
+		// input 绶忛
+		// output 鍚勯搴︺伄鏋氭暟
 		int money = 999;
 
 		if (true) {
-			// true の場合、実行
+			// true 銇牬鍚堛�佸疅琛�
 		} else {
-			// falseの場合、実行
+			// false銇牬鍚堛�佸疅琛�
+			
+			
+			//作业
 		}
 
 	}
