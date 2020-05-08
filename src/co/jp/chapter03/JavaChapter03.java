772a03537a1d39f4310063ae5391bf0f315f8f2b
BZY <zhuoyubao821@gmail.com>
2020-05-08 19:35:39 +0900

    ソース更新

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -4,17 +4,14 @@ public class JavaChapter03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		
+		// 宿題   int boolean string  6つ変数
+		
+		//成績判断表示
+		
 		//   日本硬貨　１，５，１０，５０，１００，５００
-		// input 総額
-		// output 各額度の枚数
-		int money = 999;
-
-		if (true) {
-			// true の場合、実行
-		} else {
-			// falseの場合、実行
-		}
+			// input 総額
+			// output 各額度の枚数
 
 	}
 
