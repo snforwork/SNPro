5e4b2e65212fef198cb0c52057b48cd32626590f
ASUS <ASUS@DESKTOP-42OM71Q>
2020-06-06 09:56:12 +0900

    2020.05.23(09)

--- src/co/jp/chapter06/JavaChapter06.java
+++ src/co/jp/chapter06/JavaChapter06.java
@@ -4,18 +4,18 @@ public class JavaChapter06 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		
-        //2020.05.23(2)
+
+
+        //2020.05.23(09)
 		Dog dog = new Dog();
 		dog.eat();
 		dog.say();
-		
+
 		Cat cat = new Cat();
 		cat.eat();
 		cat.say();
-		
-		
+
+
     }
-	
+
 }
