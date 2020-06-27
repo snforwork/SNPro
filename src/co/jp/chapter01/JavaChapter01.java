fa9f912501839416c49d0e6972f11ec0033aeb66
ASUS <ASUS@DESKTOP-42OM71Q>
2020-06-27 14:14:47 +0900

    Chapter01宿題提出

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -6,7 +6,7 @@ public class JavaChapter01 {
 		// TODO 自動生成されたメソッド・スタブ
 
 
-		//2020.04.11(01+02+03)
+		//2020.04.11(01)
 		//1.整数
 		int z=1;
 	    z= 1;
@@ -107,82 +107,6 @@ public class JavaChapter01 {
 	    //質問4
 	    //ブリアン（ boolean）変数に設定可能の値は"true"&"false"
 
-
-	    //02型（文字列）――質問集
-	    //質問1
-	    //System.out.println("this is line1\this line2");は何行出力しますか。１行
-
-	    //質問2
-	    //Stringを比較方法は、 equals
-
-	    //質問3
-	    //Float値⇒String文字列変更する場合、どうなメソッドを利用しますか.String.valueOf("1.0F")
-
-	    //質問4
-	    //以下式の値を記載してください.
-	    //1. 1.0 + 2.0 = 3.0
-	    //2. 1.0 + "2.0" = "1.02.0"
-
-	    //質問5
-	    int a6 =1;
-		System.out.println("a6");
-
-
-	    //03フロー制御――質問集
-	    //質問1
-	    //成績ランキング判断を作成する「90以上:A、80-89:B、70-79:C、60-69:D、60以下:E」
-	    int b1 = 56;
-	    System.out.println("成績ランキング判断、入力値:"+ b1);
-	    //以下コードを完成してください。
-	    if(a>=90){
-	    	System.out.println("A");
-	    }else if(80<=a&&a<90){
-	    	System.out.println("B");
-	    }else if(70<=a&&a<80){
-	    	System.out.println("C");
-	    }else if(60<=a&&a<70){
-	    	System.out.println("D");
-	    }else{
-	    	System.out.println("E");
-	    }
-
-	    //質問2
-	    //日本のコインを1円、5円、10円、100円、500円があります。3340円のコイン数をまとめください。
-	    //input　統額
-	    //output 各額度の枚数
-	    int money = 3340;
-	    int k = 0;
-	    int m500 = 0;
-        int m100 = 0;
-        int m50 = 0;
-        int m10 = 0;
-        int m5 = 0;
-	    int m1 = 0;
-		if(money%500>0) {
-			k = money%500;
-		    m500 = money/500;
-		    System.out.println("500円:" + m500 + "枚");
-		}if(k%100>0){
-				   m100 = k/100;
-				   k = k%100;
-				   System.out.println("100円:" + m100 + "枚");
-		}if(k%50>0){
-				   m50 = k/50;
-				   k = k%50;
-				   System.out.println("50円:" + m50 + "枚");
-		}if(k%10>0){
-				   m10 = k/10;
-				   k = k%10;
-				   System.out.println("10円:" + m10 + "枚");
-		}if(k%5>0){
-				   m5 = k/5;
-				   k = k%5;
-				   System.out.println("5円:" + m5 + "枚");
-		}if(k%1>0){
-				   m1 = k/1;
-				   k = k%1;
-				   System.out.println("1円:" + m1 + "枚");
-				   }
 
 
 
