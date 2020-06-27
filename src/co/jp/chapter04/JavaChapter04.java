63771c8db85b7cd6b7b7081cc3bb6f8476ab6e09
ASUS <ASUS@DESKTOP-42OM71Q>
2020-06-27 14:02:26 +0900

    Chapter04宿題提出

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -1,94 +1,292 @@
 package co.jp.chapter04;
 
-public class JavaChapter04{
+import java.util.Arrays;
+
+public class JavaChapter04 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+        // TODO 自動生成されたメソッド・スタブ
 
 
-		//2020.05.09(07)
-		//1.OOP
-		//青色――全局変量　赤色――内部変量
-		String strname = new String("ABC");
-		System.out.println(strname);
-
-		//インスタンス化オブジェクト（実体化対象）
-		//クラスタイプ　+ オブジェクト名称　= new コンストラクタ（構造方法）
-		Human objname = new Human();
-		objname.setName("Obama");
-		objname.setAge(50);
-		objname.setSex(1);
-
-		System.out.println(objname.getName());
-		System.out.println(objname.getAge());
-		System.out.println(objname.getSex());
-		if(objname.setSex( ) == 1) {
-			System.out.println("男性");
-		}else if(objname.setSex() == 0) {
-			System.out.println("女性");
-		}else {
-			System.out.println(objname.setSex());
-		}
+		//2020.04.18(04)
+        //1.転釈
+        String strtest="12\\\\3";
+        System.out.println("String型:"+strtest);
 
 
-		//2.引数
-		//Human trump = new Human("Trump",70,1);
-		//setなし
-		//System.out.println(trump.name);
-		//System.out.println(trump.age);
-		//if(trump.sex == 1) {
-			//System.out.println("男性");
-		//}else if(trump.sex == 0) {
-			//System.out.println("女性");
-		//}else {
-			//System.out.println(trump.sex);
-		//}
+        //2.文字列 String(アドレスと内容を比較する)
+        String strA="123";
+        String strB=new String("123");
+        System.out.println(strA==strB);//Stringの場合、==比較　アドレス＆バリュー
+        System.out.println(strA.equals(strB));//バリュー(内容)のみ比較
 
 
-		//練習1
-		//Human xiaoMing = new Human("xiaoMing",10,1);
-	    //set 無し
-		//System.out.println("xiaoMing count:"+Human.count);
-		//System.out.println(xiaoMing.getName());
-		//System.out.println(xiaoMing.getAge());
-		//if(xiaoMing.getSex() ==1) {
-		//	System.out.println("男");
-		//}else if(xiaoMing.getSex() ==2){
-		//	System.out.println("女");
-		//}else {
-		//	System.out.println(xiaoMing.getSex());
-		//}
+        //3.loop-for
+        //before
+        System.out.println("loop start");
 
-		//練習2
-		//Human obj2 = new Human("希拉里克林顿",2);
-        //System.out.println(obj2.getName());
-        //if(obj2.getSex() ==1) {
-  		//   System.out.println("男");
-  	    //}else if(obj2.getSex() ==2){
-  		//   System.out.println("女");
-  	    //}else {
-  		//   System.out.println(obj2.getSex());
-	    //}
+        //loop
+        //例:for(初期値;終了条件;自増減変量)
+        //i++ == i=i+1　
+        for(int i=0;i<10;i++) {
+            System.out.println(i);
+        }
+        //after
+        System.out.println("loop end");
 
-		//練習3
-		//Animal obj3 = new Animal();
-		//obj3.setName("Cat");
-	    //obj3.setAge(2);
-	    //obj3.setSex(1);
-	    //obj3.setLegs(3);
+        int i=0;
+        //loop　1回目(i=0)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　2回目(i=1)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　3回目(i=2)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　4回目(i=3)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　5回目(i=4)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　6回目(i=5)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　7回目(i=6)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　8回目(i=7)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　9回目(i=8)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　10回目(i=9)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
+        //loop　11回目(i=10)
+        if(i<10) {
+            System.out.println(i);
+            i=i+1;//i++ or ++i
+        }else {
+            //ループ終了;
+        }
 
-		//System.out.println(obj3.getName());
-		//System.out.println(obj3.getAge());
-		//System.out.println(obj3.getSex());
-		//if(obj3.getSex() == 1) {
-			//System.out.println("雄");
-			//}else if(obj3.getSex() == 0) {
-				//System.out.println("雌");
-			//}else {
-				//System.out.println(obj3.getSex());
-			//}
+
+        //練習1
+        //ループ15;53-39
+        System.out.println("loop start");
+         for(int i1=53;i1>=39;i1--) {
+              System.out.println(i1);
+          }
+        System.out.println("loop end");
+
+        //練習2
+        //ロジック不変;53-39
+        //方法1
+        System.out.println("loop start");
+          for(int i2=0;i2<15;i2++) {
+              int j2=53;
+              j2=j2-i2;
+              System.out.println(j2);
+          }
+        System.out.println("loop end");
+        //方法２
+        System.out.println("loop start");
+          for(int i2=0;i2<15;i2++) {
+          System.out.println(53-i2);
+          }
+        System.out.println("loop end");
+
+
+        //4.loop-for+if
+          int ateru=5;
+          for(int i3=0;i3<10;i3++) {
+              System.out.println("index="+i3);
+              if(i3==ateru) {
+                  System.out.println("数値["+i3+"]は当てる！！");
+                  break;
+              }else {
+                  System.out.println("数値["+i3+"]は当てない！！");
+                  continue;
+              }
+          }
+
+
+        //練習3
+        //日本のコインを1円、5円、10円、100円、500円があります。3348円のコイン数をまとめください。
+        int money = 3348;
+        int m500 = 0;
+        int m100 = 0;
+        int m50 = 0;
+        int m10 = 0;
+        int m5 = 0;
+        int m1 = 0;
+        for(;;) {
+            if(money>=500&&money%500!=0) {
+                m500=money/500;
+                money=money%500;
+            }else if(money>=100&&money%100!=0) {
+                m100=money/100;
+                money=money%100;
+            }else if(money>=50&&money%50!=0) {
+                m50=money/50;
+                money=money%50;
+            }else if(money>=10&&money%10!=0) {
+                m10=money/10;
+                money=money%10;
+            }else if(money>=5&&money%5!=0) {
+                m5=money/5;
+                money=money%5;
+            }else if(money>=1) {
+                m1=money/1;
+                money=money%1;
+            }
+            if(money==0) {
+                break;
+            }
+        }
+        System.out.println("500円コインが["+m500+"]枚です");
+        System.out.println("100円コインが["+m100+"]枚です");
+        System.out.println("50円コインが["+m50+"]枚です");
+        System.out.println("10円コインが["+m10+"]枚です");
+        System.out.println("5円コインが["+m5+"]枚です");
+        System.out.println("1円コインが["+m1+"]枚です");
+
+
+        //5.配列
+        int [] arr1=new int[4];
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+        System.out.println(arr1[2]);
+        System.out.println(arr1[3]);
+        //System.out.println(arr1[4]);//サイズを超える要素
+
+
+        //04配列――質問集
+        //質問1
+        int [] arr2=new int[4];
+        System.out.println(arr2[0]);
+        System.out.println(arr2[1]);
+        System.out.println(arr2[2]);
+        System.out.println(arr2[3]);
+
+        //質問2
+        int[] arr3= new int[] {5, 6};
+        System.out.println(arr3[0]);
+        System.out.println(arr3[1]);
+
+        //質問3
+        int[][] arr4 = new int[2][3];
+        arr4[0] = new int[0];
+        arr4[0] = new int[1];
+        arr4[0] = new int[2];
+        arr4[1] = new int[0];
+        arr4[1] = new int[1];
+        arr4[1] = new int[2];
+
+        //質問4
+        int[][] arr5= new int[4][2];
+        for (int[] out : arr5) {
+        	for (int inner : out) {
+        		System.out.println(inner);
+        	}
+        }
+
+        //質問5
+        //int[] arr6= new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最大値と最小桁値を System.out.println()。
+        int[] arr6= new int[] {1, 3, 5, 7, 100, 0, 1};
+        int max = 0;
+        for(int i6 = 0; i6 < arr6.length; i6++){
+            if(arr6[i6]>max) {
+               max = arr6[i6];
+               System.out.println("最大値="+max);
+            }
+        }
+
+        int min = 0;
+        for(int i5 = 0; i5 < arr6.length; i5++){
+        	if(arr6[i5]<min) {
+        		min = arr6[i5];
+        		System.out.println("最小値="+min);
+        	}
+        }
+
+        //質問6
+        //質問1の配列のSUMを求める。
+        int [] arr7=new int[4];
+        System.out.println(arr7[0]);
+        System.out.println(arr7[1]);
+        System.out.println(arr7[2]);
+        System.out.println(arr7[3]);
+        System.out.println("loop start");
+        int sum = 0;
+        for(int i7 = 0; i7 < 4; i7++){
+            sum += arr7[i7];
+            System.out.println("配列の和="+sum);
+        }
+        System.out.println("loop end");
+
+        //質問7
+        //行列（２次元配列）の和と積を計算してください。
+        int[][] arr21 = new int[][] {
+            {1,2},
+            {3,4,5}
+        };
+        int[][] arr22 = new int[][] {
+            {5,6},
+            {7,8,9}
+        };
+        for(int i1 = 0; i1 < arr21.length; i1++) {
+        	for(int j = 0; j < arr22.length; j++) {
+        	System.out.println("配列の和:" + arr21[i1][j] + arr22[i1][j] + ",");
+        	System.out.println("配列の積:" + arr21[i1][j] * arr22[i1][j] + ",");
+            }
+        System.out.println("");
+        }
 
 
 
-	}
+    }
 }
