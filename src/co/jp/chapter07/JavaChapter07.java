196b2aec41d079d17b554f07e1dd16ec90559d3c
Mloong <Mloong@DESKTOP-1LO98I2>
2020-07-04 12:40:02 +0900

    test

--- src/co/jp/chapter07/JavaChapter07.java
+++ src/co/jp/chapter07/JavaChapter07.java
@@ -90,6 +90,29 @@ public class JavaChapter07 {
            System.out.println(obj4.getLegs());
 
 
+           /**質問１*/
+   		//定義されないメソッドsetAge(1234)をアクセスする。
+//   		public class Pet {
+//   			public String name;
+//   			public void setName(String value) {
+//   			this.name = value;
+//   			}
+//   			}
+//   			// Kicker.java
+//   			public class Kicker {
+//   			public static void main(String...args) {
+//   			Pet cat = new Pet();
+//   			cat.setAge(1234);
+   //
+//   			}
+//   			}
+
+//   	/**質問２*/
+   	int[] arr = new int[] {1,5,8,6,2};
+   		System.out.println(Math.max(arr));
+   		System.out.println(Math.min(arr));
+   		System.out.println(Math.ave(arr));
+   	}
 
 
 
@@ -105,4 +128,4 @@ public class JavaChapter07 {
 
 }
 
-	}
+
---
+++ src/co/jp/chapter07/Math.java
@@ -0,0 +1,24 @@
+package co.jp.chapter07;
+
+	import java.util.Arrays;
+
+	public class Math {
+		public static int max(int[] arr) {
+	        Arrays.sort(arr);
+	        return arr[arr.length-1];
+	    }
+		public static  int min(int[] arr) {
+			Arrays.sort(arr);
+
+	        return arr[0];
+	    }
+		public static  int ave(int[] arr) {
+			int sum = 0;
+			for(int value: arr) {
+				sum+= value;
+			}
+
+			return sum/arr.length;
+	    }
+	}
+
