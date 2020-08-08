94e0d76a05466e7697f803b6ea229c31bdcb7fd3
ASUS <ASUS@DESKTOP-42OM71Q>
2020-08-08 13:35:25 +0900

    Chapter08宿題提出

--- src/co/jp/chapter08/JavaChapter08.java
+++ src/co/jp/chapter08/JavaChapter08.java
@@ -34,6 +34,46 @@ public class JavaChapter08 {
 		System.out.println(zan);
 
 
+		//宿題
+		//08OOP-その２――質問集
+		//質問1
+		//以下コードの出力するを回答してください。
+		String x = "a";
+		String y = "a";
+		System.out.println(x == y);//false
+		System.out.println(x.equals(y));//true
+		String empty = "";
+		String nullString = null;
+	    //System.out.println(nullString.equals(empty));//false
+
+		//質問2
+		//以下コードの出力するを回答してください。
+		class Employee {
+	    private String id;
+		public Employee(String id) {
+		this.id = id;
+		}
+		@Override
+		public boolean equals(Object another) {
+		if (another instanceof Employee) {
+			Employee an = (Employee) another;
+			return an.id.equals(id);
+			}
+			return false;
+		}
+		}
+		// Kicker.java
+		//public class Kicker {
+		//public static void main(String...args) {
+		//Employee x1 = new Employee("1234");
+		//Employee y1 = new Employee("1234");
+		//System.out.println(x1 == y1); //true
+		//System.out.println(x1.equals(y1)); //true
+		//}
+		//}
+
+
+
 
 
 	}
