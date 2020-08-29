b31ad4e73c79812fcd089702e8885ae8f7ee143a
ASUS <ASUS@DESKTOP-42OM71Q>
2020-08-29 13:09:34 +0900

    Chapter09宿題提出

---
+++ src/co/jp/chapter09/Animal.java
@@ -0,0 +1,42 @@
+package co.jp.chapter09;
+
+public class Animal {
+
+
+	//名前
+    String name;
+    //年齢
+  	int age;
+  	//性別 男性:1 女性:0
+  	int sex;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int setSex() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+
+
+
+
+
+
+}
--- src/co/jp/chapter09/JavaChapter09.java
+++ src/co/jp/chapter09/JavaChapter09.java
@@ -1,7 +1,9 @@
 package co.jp.chapter09;
 
-import co.jp.chapter09.Cat;
-import co.jp.chapter09.Dog;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class JavaChapter09 {
 
@@ -18,9 +20,27 @@ public class JavaChapter09 {
 		cat.eat();
 		cat.say();
 
+
 		//宿題
 		//09OOP-その3――質問集
-		//質問2︓以下ソースに匿名クラスの利用する行は?
+		//質問1: 以下ソースにラムダ式の使う場所を解釈してください。
+		class GUISample{
+			public void main(String[] args) {
+				JFrame window = new JFrame("DCNet Java　教育");
+				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				window.setSize(800,600);
+				JButton btn = new JButton("hello world");
+				window.getContentPane().add(btn);
+				//ラムダ式
+			    btn.addActionListener((ActionEvent e) -> {
+			    	System.out.println("ボタンクリックしました。");
+			    });
+
+			    window.setVisible(true);
+			}
+		}
+
+		//質問2: 以下ソースに匿名クラスの利用する行は?
 		class Kicker{
 			public void main(String...args) {
 				new Thread() {
@@ -33,33 +53,96 @@ public class JavaChapter09 {
 		}
 
 
-		//09OOP-練習問題――質問集
-		//質問１︓クラス（Class）とは︖クラス定義⽤のキーワードは「 class 」である。
-		//質問２︓クラスメンバ（Class member）の何種類ありますか。
-		//1. 属性・フィールド (Field)
-		//2. 関数(Function)・メソッド(Method)・操作(Operator)
-		//質問３︓クラスメンバの関数は特殊の関数「コンストラクタ」が持っています。
-		//質問４︓コンストラクタの特徴
-		//1. 戻り値不要
-		//2. 関数名＝クラス名称
-		//質問５︓コンストラクタの作⽤︓クラス属性の初期化する。
-		//質問６︓１つクラスに複数のコンストラクタ持つ可能。
-		//質問７︓キーワード this の使⽤⽅法︓ this.name
-		//質問８︓クラスを利⽤する為、インスタンス化する必要です。インスタンスの演算⼦は new です。
-		//質問９︓演算⼦ new の直後呼び出しているメソッドは「コンストラクタ」︔
-		//質問１０︓アクセス制御の３種類あります。
-		//1. + public
-		//2. # protected
-		//3. - private
-		//質問１１︓静的のメンバの定義するキーワード（修飾⼦）は static 。
-		//質問１２︓静的のメンバアクセス時、インスタンス化不要。アクセス⽅法は「クラス名.静的のメン バ」
-		//質問１５︓クラス継承する⽤キーワードは「 extends 」
-		//質問１６︓継承しているクラス。上位クラスは「親クラス」・「スーパークラス」。下位クラスは 「サブクラス」と呼びます。
-		//質問１７︓サブクラス＆アクセス制御（ public, protected ）。
-		//質問１８︓サブクラスと親クラス同じメソッド名かつメソッドの引数は⼀致する場合、「オーバー ライド」となります。
-		//質問１９︓以下Animalクラスを継承して以下２クラスを作成してください。
-		//1. クラス Cat。sayHelloメソッドをオーバーライドして、System.out.println(“ニャー“);
-		//2. クラス Dog。sayHelloメソッドをオーバーライドして、System.out.println(“ワン“);
+		//09OOP-練習問題————質問集
+	    //質問1: クラス（Class）とは？クラス定義用のキーワードは「 class 」である。
+
+	    //質問2: クラスメンバ（Class member）の何種類ありますか。
+		  //1. 属性・フィールド (Field)
+		  //2. 関数(Function)・メソッド(Method)・操作(Operator)
+
+		//質問3： クラスメンバの関数は特殊の関数「コンストラクタ」が持っています。
+
+		//質問4： コンストラクタの特徴
+		  //1. 戻り値不要
+		  //2. 関数名＝クラス名称
+
+		//質問5： コンストラクタの作用: クラス属性の初期化する。
+
+	    //質問6： 1つクラスに複数のコンストラクタ持つ可能。
+
+		//質問7： キーワード this の使用方法: this.name
+	      class People{
+	    	  private String name;
+	    	  public void setName(String name) {
+	    		  this.name = name;
+	    	  }
+	      }
+
+		//質問8： クラスを利用料する為、インスタンス化する必要です。インスタンスの演算子は new です。
+
+		//質問9： 演算子は new の直後呼び出しているメソッドは「コンストラクタ」;
+
+		//質問10： アクセス制御の3種類あります。
+		  //1. + public
+		  //2. # protected
+		  //3. - private
+
+		//質問11： 静的のメンバの定義するキーワード（修飾子）は static 。
+
+		//質問12： 静的のメンバアクセス時、インスタンス化不要。アクセス方法は「クラス名.静的のメンバ」。
+	      //1.System.out.println("1234");// 静的のField
+	      //2.String value = String.format("%05d".0);//静的のメソッド
+
+	    //質問13： クラス「Animal」を定義して、インスタンス化しオブジェクトcatを作成します。変数catのsayHello()メソッドを呼び出してください。
+	      //Animal objname = new Animal();
+			//objname.setName("Cat");
+			//objname.setAge(5);
+			//objname.setSex(1);
+			//System.out.println(objname.getName());
+			//System.out.println(objname.getAge());
+			//System.out.println(objname.getSex());
+			//if(objname.setSex() == 1) {
+				//System.out.println("男性");
+			//}else if(objname.setSex() == 0) {
+				//System.out.println("女性");
+			//}else {
+				//System.out.println(objname.setSex());
+			//}
+			//class Cat{
+				//public void say() {
+				//TODO 自動生成されたメソッド・スタブ
+					//System.out.println("hello");
+				//}
+			//}
+
+		//質問14： 以下1行を解釈してください。
+		  //1.System.out.println(new Animal("cat").getName());
+		  //変量「new Animal("cat")」は自分の名称を返す。
+
+		//質問15:  クラス継承する用キーワードは「 extends 」。
+
+		//質問16： 継承しているクラス。上位クラスは「親クラス」・「スーパークラス」。下位クラスは「サブクラス」と呼びます。
+
+		//質問17： サブクラス＆アクセス制御（ public, protected ）。
+
+		//質問18： サブクラスと親クラス同じメソッド名かつメソッドの引数は一致する場合、「オーバーライド」となります。
+
+		//質問19： 以下Animalクラスを継承して以下2クラスを作成してください。
+		  //1. クラス Cat。sayHelloメソッドをオーバーライドして、System.out.println("ニャー");
+		  //2. クラス Dog。sayHelloメソッドをオーバーライドして、System.out.println("ワン");
+
+		//※親クラスに明示的なコンストラクタが存在しない場合、サブクラスは必ず親クラスのコンストラクタをオーバーライドします。
+
+		//質問20:  以下コードをIS-A関係で説明してください。
+		  //1. Animal v0 = new Animal();//OK: new Animal は Animal。
+		  //2. Animal v1 = new Cat("a cat");//OK: new Cat は Animal。
+          //3. Animal v2 = new Dog("a dog");//OK: new Dog は Animal。
+          //5. Cat v3 = new Cat("");//OK: new Cat は Cat。
+		  //6. Dog v4 = new Dog("");//OK: new Dog は Dog。
+		  //8. Cat v5 = new Animal("");//NG: new Animal は Cat。
+
+
+
 
 
 
