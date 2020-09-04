4e268cbe3ccc3a075afbca64495c6d131dfc6f72
Mloong <Mloong@DESKTOP-1LO98I2>
2020-09-04 10:34:58 +0900

    test

---
+++ src/co/jp/chapter14/Chapter14.java
@@ -0,0 +1,74 @@
+
+package co.jp.chapter14;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class Chapter14 {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO 自動生成されたメソッド・スタブ
+//質問１︓以下共通メソッドを作成してください。
+		Calendar c = Calendar.getInstance();
+
+		DateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
+		Date beginDate = dateFormat1.parse("2014-05-01");
+		Date endDate = dateFormat1.parse("2014-06-25");
+		Date date= beginDate;
+		while(!date.equals(endDate)) {
+			System.out.println(date);
+			c.setTime(date);
+			c.add(Calendar.DATE,1);
+			date = c.getTime();
+		}
+		 //start时间
+//		  String str="2017-08-05";
+		  //end时间
+//		  String str1="2017-08-10";
+//		  String str="2015-08-31 21:08:06";
+//          SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//          Date date = (Date) sdf.parse(str);
+
+//		Date currentDate = new Date();
+//		System.out.println(currentDate);
+//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//		Calendar start = Calendar.getInstance();
+//		Calendar end = Calendar.getInstance();
+//		Calendar calendar = Calendar.getInstance();
+//		Date date = (Date) format.parse(str);
+//		 System.out.println(date.getTime());
+//		start.setTime(str1);
+//		System.out.println(start);
+//		try {
+//			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+//			Date date1 = (Date)format.parse(str);
+//			Date date2 = format.parse(str1);
+
+//			System.out.println(date);
+//			} catch (ParseException ex) {
+//			Logger.getLogger(FileStreamSample.class.getName()).log(Level.SEVERE, null, ex);
+//			}
+//		TRY {
+//		    START.SETTIME(FORMAT.PARSE(STR));
+//		    END.SETTIME(FORMAT.PARSE(STR1));
+//		  } CATCH (JAVA.TEXT.PARSEEXCEPTION E) {
+//		    E.PRINTSTACKTRACE();
+//		  }
+//		while(date1.before(date2))
+//		  {
+//			 start.add(Calendar.DAY_OF_MONTH,1);
+//		      System.out.println(start.getTime());
+//
+//		  }
+//		DateFormat beginDate= new SimpleDateFormat("2020-06-25");
+//		DateFormat endDate= new SimpleDateFormat("2020-06-29");
+//		DateFormat date = beginDate;
+//		if (!date.equals(endDate)) {
+//	        System.out.println(beginDate);
+//	    }else {
+//	    	System.out.println(dateString);
+	    }}
---
+++ src/co/jp/chapter15/Chapter15.java
@@ -0,0 +1,66 @@
+package co.jp.chapter15;
+
+import java.util.Scanner;
+
+public class Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//ユーザーが入力された内容取得
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		//ユーザーの口座番号、パスワード、名義人と金額を声明する
+		String[]cardNub  ={"111","1111","111"};
+		String[] pwdNum= {"112","223","343"};
+		String[] user= {"木村拓哉","石原さとみ","小栗旬"};
+		double[] money= {10000000.0,20000000.0,50000000.0};
+		//password入力された回数
+		int time = 3;
+		//カード番号存在するか判断、２０の場合存在していない
+		int index1=2;
+//		int index2=2;
+		//loop終了判断、３の場合loop終了
+		int out = 3;
+		do {
+			System.out.println("口座番号を入力してください");
+			String cardNumber = scanner.nextLine();
+			for(int i=0;i<cardNub.length+1;i++) {
+				if(cardNumber.equals(cardNub[i])) {
+					index1=i;
+					break;
+				}
+			}
+			if(index1!=2) {
+				System.out.println("パスワードを入力してください");
+				String passWord = scanner.nextLine();
+				if(passWord.equals(pwdNum[index1])) {
+					System.out.println("Successful!");
+					while(out==3) {
+						System.out.println("1.引き出し　2.預かり　3.残高照会");
+						int num = scanner.nextInt();
+						switch(num) {
+
+						case 1:System.out.println("引き出す金額を入力してください：");
+							int getMoney = scanner.nextInt();
+							if(getMoney>money[index1]) {
+								System.out.println("NoPossible!");
+							}else {
+								money[index1] = getMoney;
+								}
+						case 2:System.out.println("預かる金額を入力してください：");
+							int saveMoney=scanner.nextInt();
+							money[index1]+=saveMoney;
+							System.out.println(money[index1] + saveMoney);
+
+						case 3:System.out.println(money[index1]);
+							break;
+						}
+					};
+				}
+			}else {
+				System.out.println("入力された口座番号、又はパスワードが誤っています。");
+			}
+		}while (time>0);
+	}
+
+}
